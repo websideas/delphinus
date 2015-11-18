3af19d76c4e771ba72974598ee432676d3510783
websideas <websideas.corp@gmail.com>
2015-11-18 15:20:56 +0700

    Add my account

---
+++ woocommerce-myaccount.php
@@ -0,0 +1,37 @@
+<?php
+global $shadow;
+$shadow = ' ';
+
+include_once('templates/headers/head.php');
+include_once('templates/headers/header1.php');
+
+?>
+    <div class="page-section bg-gray small-section">
+        <div class="page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <ol class="breadcrumb">
+                            <li><a href="#">Home</a></li>
+                            <li class="active">My account</li>
+                        </ol>
+                    </div>
+                    <div class="col-md-8 text-right">
+                        <h1>My account</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="main">
+
+
+
+
+    </div><!-- #main -->
+<?php
+
+include_once('templates/footers/footer3.php');
+include_once('templates/footers/footer.php');
+
+
