05e2855b3592618f81ec72413ee890692a27f4e9
tienthangtn <tienthangk43@gmail.com>
2015-11-23 10:22:04 +0700

    update slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -775,6 +775,89 @@
             }
         });
 
+        $("#rev_slider_3").show().revolution({
+            sliderType:"standard",
+            jsFileLocation:"assets/libs/revolution/js/",
+            sliderLayout:"fullwidth",
+            dottedOverlay:"none",
+            delay:6000,
+            navigation: {
+                keyboardNavigation:"off",
+                keyboard_direction: "horizontal",
+                mouseScrollNavigation:"off",
+                onHoverStop:"off",
+                touch:{
+                    touchenabled:"on",
+                    swipe_threshold: 75,
+                    swipe_min_touches: 50,
+                    swipe_direction: "horizontal",
+                    drag_block_vertical: false
+                }
+                ,
+                arrows: {
+                    style:"site-nav",
+                    enable:true,
+                    hide_onmobile:true,
+                    hide_under:600,
+                    hide_onleave:true,
+                    hide_delay:200,
+                    hide_delay_mobile:1200,
+                    tmp:'',
+                    left: {
+                        h_align:"left",
+                        v_align:"center",
+                        h_offset:30,
+                        v_offset:0
+                    },
+                    right: {
+                        h_align:"right",
+                        v_align:"center",
+                        h_offset:30,
+                        v_offset:0
+                    }
+                }
+                ,
+                bullets: {
+                    enable:true,
+                    hide_onmobile:true,
+                    hide_under:600,
+                    style:"site-pagination",
+                    hide_onleave:true,
+                    hide_delay:200,
+                    hide_delay_mobile:1200,
+                    direction:"horizontal",
+                    h_align:"center",
+                    v_align:"bottom",
+                    h_offset:0,
+                    v_offset:30,
+                    space: 10,
+                    tmp:'<span class="tp-bullet-inner"></span>'
+                }
+            },
+            gridwidth:1290,
+            gridheight:920,
+            lazyType:"none",
+            shadow:0,
+            spinner:"spinner0",
+            stopLoop:"off",
+            stopAfterLoops:-1,
+            stopAtSlide:-1,
+            shuffle:"off",
+            autoHeight:"off",
+            disableProgressBar:"off",
+            hideThumbsOnMobile:"off",
+            hideSliderAtLimit:0,
+            hideCaptionAtLimit:0,
+            hideAllCaptionAtLilmit:0,
+            startWithSlide:0,
+            debugMode:false,
+            fallbacks: {
+                simplifyAll:"off",
+                nextSlideOnWindowFocus:"off",
+                disableFocusListener:false
+            }
+        });
+
 
         $("#rev_slider_2_1").show().revolution({
             sliderType:"standard",
