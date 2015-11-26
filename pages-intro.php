1efa38bb53e2730ca35483ee8a7593b5d631300e
websideas <websideas.corp@gmail.com>
2015-11-26 11:48:41 +0700

    Change

--- pages-intro.php
+++ pages-intro.php
@@ -4,62 +4,74 @@ include_once('templates/headers/head.php');
 include_once('templates/headers/header3.php');
 
 ?>
-
-
-<div id="main" class="bg-gray">
-    
-    <div class="intro-header">
-        <div class="intro-content">
-            <h1>new way to design</h1>
-            <p>Dephinus with a tool that makes design <br />simple for everyone.</p>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row demo-item-wrapper">
-            <div class="col-md-6 demo-item">
-                <a href="index.php">
-                    <img class="img-responsive" src="assets/images/home1.jpg" alt="" />
-                    <h5>Home version 1</h5>
-                </a>
-            </div>
-            <div class="col-md-6 demo-item">
-                <a href="homepage2.php">
-                    <img class="img-responsive" src="assets/images/home2.jpg" alt="" />
-                    <h5>Home version 2</h5>
-                </a>
-            </div>
-            <div class="col-md-6 demo-item">
-                <a href="homepage3.php">
-                    <img class="img-responsive" src="assets/images/home3.jpg" alt="" />
-                    <h5>Home version 3</h5>
-                </a>
-            </div>
-            <div class="col-md-6 demo-item">
-                <a href="homepage4.php">
-                    <img class="img-responsive" src="assets/images/home4.jpg" alt="" />
-                    <h5>Home version 4</h5>
-                </a>
-            </div>
-            <div class="col-md-6 demo-item">
-                <a href="homepage5.php">
-                    <img class="img-responsive" src="assets/images/home5.jpg" alt="" />
-                    <h5>Home version 5</h5>
-                </a>
-            </div>
-            <div class="col-md-6 demo-item">
-                <a href="homepage6.php">
-                    <img class="img-responsive" src="assets/images/home6.jpg" alt="" />
-                    <h5>Home version 6</h5>
-                </a>
+    <section class="cd-intro">
+        <div class="cd-intro-content mask">
+            <h1 data-content="new way to design"><span>new way to design</span></h1>
+            <div class="action-wrapper">
+                <p>Dephinus with a tool that makes design simple <br>for everyone.</p>
             </div>
         </div>
-    </div>
-
-</div><!-- #main -->
-
-
-
+    </section>
+    <div id="main">
+        <div class="page-section bg-gray">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6 ">
+                        <div class="demo-item demo-item-left">
+                            <a href="http://kitethemes.com/delphinus/html/index.php">
+                                <span style="background-image:url(assets/images/Home_01.jpg);" class="img-wrap move-long"></span>
+                            </a>
+                            <h2><a href="http://kitethemes.com/delphinus/html/index.php"> Home version 1 </a></h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="demo-item demo-item-right">
+                            <a href="http://kitethemes.com/delphinus/html/homepage2.php">
+                                <span style="background-image:url(assets/images/Home_02.jpg);" class="img-wrap move-long"></span>
+                            </a>
+                            <h2><a href="http://kitethemes.com/delphinus/html/homepage2.php">Home version 2</a></h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 ">
+                        <div class="demo-item demo-item-left">
+                            <a href="http://kitethemes.com/delphinus/html/homepage3.php">
+                                <span style="background-image:url(assets/images/Home_03.jpg);" class="img-wrap move-long"></span>
+                            </a>
+                            <h2><a href="http://kitethemes.com/delphinus/html/homepage3.php">Home version 3</a></h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="demo-item demo-item-right">
+                            <a href="http://kitethemes.com/delphinus/html/homepage4.php">
+                                <span style="background-image:url(assets/images/Home_04.jpg);" class="img-wrap move-long"></span>
+                            </a>
+                            <h2><a href="http://kitethemes.com/delphinus/html/homepage4.php">Home version 4</a></h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 ">
+                        <div class="demo-item demo-item-left">
+                            <a href="http://kitethemes.com/delphinus/html/homepage5.php">
+                                <span style="background-image:url(assets/images/Home_05.jpg);" class="img-wrap move-long"></span>
+                            </a>
+                            <h2><a href="http://kitethemes.com/delphinus/html/homepage5.php">Home version 5</a></h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="demo-item demo-item-right">
+                            <a href="http://kitethemes.com/delphinus/html/homepage6.php">
+                                <span style="background-image:url(assets/images/Home_06.jpg);" class="img-wrap move-long"></span>
+                            </a>
+                            <h2><a href="http://kitethemes.com/delphinus/html/homepage6.php">Home version 6</a></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- #main -->
 <?php
 
 include_once('templates/footers/foot.php');
