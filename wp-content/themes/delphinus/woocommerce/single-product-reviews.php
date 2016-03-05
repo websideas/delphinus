b94405bfe4a1f3efdb416c951c1eb5a7ec23b3bb
websideas <websideas.corp@gmail.com>
2016-03-05 13:48:11 +0700

    Add

---
+++ wp-content/themes/delphinus/woocommerce/single-product-reviews.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Display single product reviews (comments)
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product-reviews.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+ * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+ * as little as possible, but it does happen. When this occurs the version of the template file will.
+ * be bumped and the readme will list any important changes.
+ *
+ * @see 	    http://docs.woothemes.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.3.2
+ */
+global $product;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! comments_open() ) {
+	return;
+}
+
+?>
+<div id="reviews" class="row">
+	<div id="comments" class="col-md-6">
+		<h3 class="comments-title"><?php
+			if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' && ( $count = $product->get_review_count() ) )
+				printf( _n( '%s review', '%s reviews', $count, 'delphinus' ), $count );
+			else
+				_e( 'Reviews', 'woocommerce' );
+		?></h3>
+
+		<?php if ( have_comments() ) : ?>
+
+			<ol class="commentlist">
+				<?php wp_list_comments( apply_filters( 'woocommerce_product_review_list_args', array( 'callback' => 'woocommerce_comments' ) ) ); ?>
+			</ol>
+
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+				echo '<nav class="woocommerce-pagination">';
+				paginate_comments_links( apply_filters( 'woocommerce_comment_pagination_args', array(
+					'prev_text' => '&larr;',
+					'next_text' => '&rarr;',
+					'type'      => 'list',
+				) ) );
+				echo '</nav>';
+			endif; ?>
+
+		<?php else : ?>
+
+			<p class="woocommerce-noreviews"><?php _e( 'There are no reviews yet.', 'woocommerce' ); ?></p>
+
+		<?php endif; ?>
+	</div>
+
+	<?php if ( get_option( 'woocommerce_review_rating_verification_required' ) === 'no' || wc_customer_bought_product( '', get_current_user_id(), $product->id ) ) : ?>
+
+		<div id="review_form_wrapper" class="col-md-5 col-md-offset-1">
+			<div id="review_form">
+				<?php
+					$commenter = wp_get_current_commenter();
+
+					$comment_form = array(
+						'title_reply'          => have_comments() ? __( 'Here you can review this item.', 'delphinus' ) : sprintf( __( 'Be the first to review &ldquo;%s&rdquo;', 'woocommerce' ), get_the_title() ),
+						'title_reply_to'       => __( 'Leave a Reply to %s', 'woocommerce' ),
+						'comment_notes_before' => '',
+						'comment_notes_after'  => '',
+                        'title_reply_before'   => '<h3 id="reply-title" class="comment-reply-title comments-title">',
+						'fields'               => array(
+							'author' => '<p class="comment-form-author col-sm-12">' .
+							            '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" placeholder="'.__( 'Name', 'woocommerce' ).' *'.'" aria-required="true" /></p>',
+							'email'  => '<p class="comment-form-email col-sm-12">' .
+							            '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '"  placeholder="'.__( 'Email', 'woocommerce' ).' *'.'"  size="30" aria-required="true" /></p>',
+						),
+						'label_submit'  => __( 'Submit', 'woocommerce' ),
+						'logged_in_as'  => '',
+						'comment_field' => '<p class="comment-form-comment"><textarea placeholder="'.__( 'Your Review', 'woocommerce' ).'" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+                        'submit_button' => '',
+                        'class_submit'  => 'btn btn-block btn-dark-border',
+					);
+
+					if ( $account_page_url = wc_get_page_permalink( 'myaccount' ) ) {
+						$comment_form['must_log_in'] = '<p class="must-log-in">' .  sprintf( __( 'You must be <a href="%s">logged in</a> to post a review.', 'woocommerce' ), esc_url( $account_page_url ) ) . '</p>';
+					}
+
+					if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' ) {
+                        $comment_form['submit_button'] = '<span class="comment-form-rating"><label for="rating">' . __( 'Your Rating', 'woocommerce' ) .'</label><select name="rating" id="rating">
+							<option value="">' . __( 'Rate&hellip;', 'woocommerce' ) . '</option>
+							<option value="5">' . __( 'Perfect', 'woocommerce' ) . '</option>
+							<option value="4">' . __( 'Good', 'woocommerce' ) . '</option>
+							<option value="3">' . __( 'Average', 'woocommerce' ) . '</option>
+							<option value="2">' . __( 'Not that bad', 'woocommerce' ) . '</option>
+							<option value="1">' . __( 'Very Poor', 'woocommerce' ) . '</option>
+						</select></span>';
+					}
+
+                    $comment_form['submit_button'] .= '<span class="comment-form-button"><button name="%1$s" type="submit" id="%2$s" class="%3$s">%4$s</button></span>';
+
+					comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) );
+				?>
+			</div>
+		</div>
+
+	<?php else : ?>
+
+		<p class="woocommerce-verification-required col-md-offset-1 col-md-5"><?php _e( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' ); ?></p>
+
+	<?php endif; ?>
+
+	<div class="clear"></div>
+</div>
